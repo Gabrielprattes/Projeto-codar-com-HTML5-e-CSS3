49ea77a4a1658c2ac64f6cf1e5155c81d68f0385
Gabrielprattes <gabrielferrandoprattes@gmail.com>
2021-05-30 13:08:24 -0300

    Versionamento projeto codar
    versionameto dos arquivos do projeto

---
+++ projeto-codar/codar/const valores = [7.7, 6.6, 5.5, 4.4].js
@@ -0,0 +1,13 @@
+const valores = [7.7, 6.6, 5.5, 4.4]
+console.log(valores[0], valores[3])
+console.log(valores[4])
+
+console.log(valores.length)
+console.log(valores.pop())
+
+console.log(valores)
+delete valores[0]
+
+console.log(valores)
+
+console.log(typeof valores)
